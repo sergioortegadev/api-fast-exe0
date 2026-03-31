dd9f5f7a0a381cebd676353d7b5068bd68b4b348
Sergio Ortega <sergioortegadev@gmail.com>
2026-03-30 21:23:06 -0300

    feat: Td002 SQLite Turso integrated: update db config and model, only ventas table work

--- src/models/ventasModel.js
+++ src/models/ventasModel.js
@@ -1,17 +1,26 @@
-import pool from "../config/db.js";
+import db from "../config/db.js";
 
 export const ventasModel = {
   getAll: async () => {
-    const [rows] = await pool.query("SELECT * FROM ventas");
-    return rows;
+    const result = await db.execute("SELECT * FROM ventas");
+    return result.rows;
   },
   createVentas: async (producto, monto) => {
-    return await pool.query("INSERT INTO ventas (producto, monto) VALUES (?, ?)", [producto, monto]);
+    return await db.execute({
+      sql: "INSERT INTO ventas (producto, monto) VALUES (?, ?)",
+      args: [producto, monto],
+    });
   },
   updateVentas: async (monto, id) => {
-    return await pool.query("UPDATE ventas SET monto = ? WHERE id = ?", [monto, id]);
+    return await db.execute({
+      sql: "UPDATE ventas SET monto = ? WHERE id = ?",
+      args: [monto, id],
+    });
   },
   deleteVentas: async (id) => {
-    return await pool.query("DELETE FROM ventas WHERE id = ?", [id]);
+    return await db.execute({
+      sql: "DELETE FROM ventas WHERE id = ?",
+      args: [id],
+    });
   },
 };
